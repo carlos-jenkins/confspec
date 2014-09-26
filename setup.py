a1f56a750af8bb2cc865eb9dd992600d4c4321e2
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2014-09-25 18:51:51 -0600

    No testing for 3.2.

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'Operating System :: Unix',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',

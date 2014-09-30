6277b43bce7a0d99e405662ce4d45beabc72c911
Carlos Miguel Jenkins Pérez <carlos@jenkins.co.cr>
2014-09-29 21:08:05 -0600

    I give up, RTD doesn't allow a sh*t.

--- doc/conf.py
+++ doc/conf.py
@@ -275,13 +275,21 @@ intersphinx_mapping = {
 
 # -- Custom theme options -----------------------------------------------------
 
-try:
-    from sphinx_bootstrap_theme import get_html_theme_path as bootstrap_path
-    pygments_style = 'monokai'
-    html_theme = 'bootstrap'
-    html_theme_options = {
-        'bootswatch_theme': 'flatly',
-    }
-    html_theme_path = bootstrap_path()
-except ImportError:
-    print('Unable to import sphinx_bootstrap_theme :(')
+# Override variables in on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# try:
+#     from sphinx_bootstrap_theme import get_html_theme_path as bootstrap_path
+#     pygments_style = 'monokai'
+#     html_theme = 'bootstrap'
+#     html_theme_options = {
+#         'bootswatch_theme': 'flatly',
+#     }
+#     html_theme_path = bootstrap_path()
+# except ImportError:
+#     print('Unable to import sphinx_bootstrap_theme :(')
